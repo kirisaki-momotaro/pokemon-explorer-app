b856853bdf5f3f6d168c32c3bbe35dea26bc0c9b
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2025-02-18 14:14:55 +0200

    added glow effect to the pokedex button

--- pokemon_explorer_app/lib/components/pokedex_header_button.dart
+++ pokemon_explorer_app/lib/components/pokedex_header_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_glow/flutter_glow.dart';
 
 class PokedexHeaderButton extends StatelessWidget {
   final double size;
@@ -9,35 +10,43 @@ class PokedexHeaderButton extends StatelessWidget {
     final String? currentRoute = ModalRoute.of(context)?.settings.name;
     final bool showBackButton = currentRoute != '/';
 
-    return Container(
-      width: size * 0.85,
-      height: size * 0.85,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(color: Colors.black, width: 3),
-        color: const Color(0xFF8CF2F5),
-      ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          
-          if (showBackButton)
-            SizedBox.expand(
-              child: Material(
-                color: Colors.transparent, // Prevents background color
-                child: InkWell(
-                  borderRadius:
-                      BorderRadius.circular(size * 0.5), // Keep circular shape
-                  onTap: () => Navigator.pop(context),
-                  child: Center(
-                    child:
-                        Icon(Icons.arrow_back, size: size * 0.5, color: Colors.black),
+    return GlowContainer(
+      glowColor: const Color(0xFF8CF2F5), 
+      blurRadius: 12, 
+      shape: BoxShape.circle,
+      child: Container(
+        width: size * 0.85,
+        height: size * 0.85,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.black, width: 3),
+          color: const Color(0xFF8CF2F5),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            if (showBackButton)
+              SizedBox.expand(
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(size * 0.5),
+                    onTap: () => Navigator.pop(context),
+                    child: Center(
+                      child: GlowIcon(
+                        Icons.arrow_back,
+                        size: size * 0.5,
+                        color: Colors.white,
+                        glowColor: Colors.red,
+                        blurRadius: 9, 
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
             _buildWhiteCircle(size),
-        ],
+          ],
+        ),
       ),
     );
   }
